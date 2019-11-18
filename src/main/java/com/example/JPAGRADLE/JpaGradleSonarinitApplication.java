663f7e7b94338824c4afb5f584f96ef2c7d3d77c
Anwar Mulla <007anwar@github.com>
2019-11-18 11:24:47 +0530

    project initialized

---
+++ src/main/java/com/example/JPAGRADLE/JpaGradleSonarinitApplication.java
@@ -0,0 +1,13 @@
+package com.example.JPAGRADLE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaGradleSonarinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaGradleSonarinitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JPAGRADLE/JpaGradleSonarinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JPAGRADLE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaGradleSonarinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
