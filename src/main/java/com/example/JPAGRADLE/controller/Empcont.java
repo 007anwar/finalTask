858685fe33c366bc10c4a2295952fed14189fc84
Anwar Mulla <007anwar@github.com>
2019-11-18 11:45:31 +0530

    initialized pojo

---
+++ src/main/java/com/example/JPAGRADLE/controller/Empcont.java
@@ -0,0 +1,7 @@
+package com.example.JPAGRADLE.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Empcont {
+}
---
+++ src/main/java/com/example/JPAGRADLE/pojo/Employee.java
@@ -0,0 +1,52 @@
+package com.example.JPAGRADLE.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "employeename", nullable = false)
+	private String name;
+	@Column(name = "email", nullable = false)
+	private String email;
+	@Column(name = "Desg", nullable = false)
+	private String designation;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+}
